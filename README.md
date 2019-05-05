3347e551b52f81c75d332b43946af54033c62aaa
iota9star <iota9star@gmail.com>
2019-05-05 10:13:38 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,64 @@
+# qiniu-upload-plugin-pro
+
+**将 webpack 打包出来的文件上传到七牛云。**
+
+> 该项目基于[yhlben/qiniu-upload-plugin](https://github.com/yhlben/qiniu-upload-plugin)以及[七牛Node.js SDK](https://developer.qiniu.com/kodo/sdk/1289/nodejs)实现
+
+## 特点
+
+- 支持上传webpack打包的文件
+- 支持配置忽略文件正则
+- 支持覆盖已上传文件
+- 支持指定文件路径前缀
+- 支持上传前删除指定前缀文件列表
+- 支持指定上传文件的文件类型
+
+## 安装
+
+```js
+npm install qiniu-upload-plugin-pro --save-dev
+```
+
+## 使用方法
+
+```js
+const QiniuUploadPluginPro = require('./QiniuUploadPluginPro');
+
+plugins: [
+  new QiniuUploadPluginPro({
+    excludeRegex: /.html$/,// 需要排除文件的正则
+    publishPath: 'http://cdn.xxx.com', // 七牛云域名，自动替换 publicPath
+    accessKey: 'your qiniu accessKey', // 个人中心，秘钥管理，AK
+    secretKey: 'your qiniu secretKey', // 个人中心，秘钥管理，SK
+    bucket: 'your qiniu bucket', // 存储空间名称
+    zone: 'Zone_z2', // 存储地区
+    // 可选参数：
+    cover: false, // 默认为 false，设置为 true 会覆盖掉已经保存在七牛云上的同名文件
+    deleteBefore: true,// 是否在上传前删除指定前缀文件列表
+    pathPrefix: 'test/upload',// 文件前缀，上传文件路径 test/upload/你的文件路径,
+    mimeType: {
+      '.js': 'application/javascript',
+      '.css': 'text/css',
+      '.ttf': 'font/ttf',
+      '.jpg': 'image/jpeg',
+      '.png': 'image/png',
+      '.gif': 'image/gif',
+      '.bmp': 'image/bmp',
+      '.txt': 'text/plain',
+      '.doc': 'application/msword',
+      '.xls': 'application/vnd.ms-excel',
+      '.xml': 'text/xml',
+      '.apk': 'application/vnd.android.package-archive',
+      '.svg': 'image/svg+xml',
+      '.map': 'application/x-navimap',
+      '.ico': 'image/x-icon',
+      '.woff': 'font/woff',
+      '.eot': 'font/eot',
+      '.webp': 'image/webp'
+    }// 上面为内置的文件类型映射，如果你的文件类型不在此列，请自己添加，将会和默认的合并，如果在可以忽略此配置项
+  })
+];
+```
+## 示例
+ Vue cli 3配置
+ ![示例](https://ws1.sinaimg.cn/large/a7bfec21gy1g2q7uo84l8j20hc0e9glz.jpg)
---
+++ src/QiniuUploadPluginPro.js
@@ -0,0 +1,274 @@
+const qiniu = require('qiniu');
+const path = require('path');
+const ora = require('ora');
+
+// 上传文件到七牛云
+class QiniuUploadPluginPro {
+  constructor(qnConfig) {
+    if (!qnConfig || !qnConfig.publicPath || !qnConfig.accessKey || !qnConfig.secretKey || !qnConfig.bucket || !qnConfig.zone) {
+      throw '缺失必要参数！';
+    }
+    // 保存用户传参
+    this.config = qnConfig;
+    if (!this.config.pathPrefixLimit || this.config.pathPrefixLimit == null || this.config.pathPrefixLimit <= 0) {
+      this.config.pathPrefixLimit = 99999999;// 设置默认值
+    }
+    // 鉴权
+    this.mac = new qiniu.auth.digest.Mac(qnConfig.accessKey, qnConfig.secretKey);
+    // 设置存储空间名称
+    const options = {scope: qnConfig.bucket};
+    // 创建上传token
+    const putPolicy = new qiniu.rs.PutPolicy(options);
+    this.uploadToken = putPolicy.uploadToken(this.mac);
+    let config = new qiniu.conf.Config();
+    // 存储空间对应的机房
+    config.zone = qiniu.zone[qnConfig.zone];
+    this.formUploader = new qiniu.form_up.FormUploader(config);
+    this.putExtra = new qiniu.form_up.PutExtra();
+    this.bucketManager = new qiniu.rs.BucketManager(this.mac, config);
+    let mimeType = {
+      '.js': 'application/javascript',
+      '.css': 'text/css',
+      '.ttf': 'font/ttf',
+      '.jpg': 'image/jpeg',
+      '.png': 'image/png',
+      '.gif': 'image/gif',
+      '.bmp': 'image/bmp',
+      '.txt': 'text/plain',
+      '.doc': 'application/msword',
+      '.xls': 'application/vnd.ms-excel',
+      '.xml': 'text/xml',
+      '.apk': 'application/vnd.android.package-archive',
+      '.svg': 'image/svg+xml',
+      '.map': 'application/x-navimap',
+      '.ico': 'image/x-icon',
+      '.woff': 'font/woff',
+      '.eot': 'font/eot',
+      '.webp': 'image/webp'
+    };
+    try {
+      let customMimeType = qnConfig.mimeType;// 合并自定义的文件类型
+      if (customMimeType) {
+        for (let k in customMimeType) {
+          if (customMimeType.hasOwnProperty(k)) {
+            mimeType[k] = customMimeType[k]
+          }
+        }
+      }
+    } catch (e) {
+      console.log(e)
+    }
+    this.mimeType = mimeType;
+    this.waitDeleteList = [];
+    this.waitUploadList = [];
+    this.waitChangeMimeTypeList = [];
+  }
+
+  apply(compiler) {
+    compiler.hooks.compilation.tap('QiniuUploadPluginPro', compilation => {
+      if (this.config.pathPrefix && this.config.pathPrefix != null && this.config.pathPrefix !== '') {
+        compilation.outputOptions.publicPath = this.config.publicPath + this.config.pathPrefix;
+      } else {
+        compilation.outputOptions.publicPath = this.config.publicPath;
+      }
+      this.absolutePath = compilation.outputOptions.path;
+    });
+    compiler.hooks.done.tapAsync('QiniuUploadPluginPro', (data, callback) => {
+      // 先返回构建结果，然后异步上传
+      callback();
+      console.log('\n\n开始七牛云插件任务...\n\n');
+      Object.keys(data.compilation.assets).forEach(file => {
+        if (this.config.excludeRegex) {
+          if (!this.config.excludeRegex.test(file)) {
+            this.waitUploadList.push(file);
+          }
+        } else {
+          this.waitUploadList.push(file);
+        }
+      });
+      if (this.config.deleteBefore) {
+        this.getPathPrefixFiles()
+      } else {
+        this.startUpload(data)
+      }
+    });
+  }
+
+  chunk(arr, size) {
+    let result = [];
+    for (let i = 0; i < arr.length; i = i + size) {
+      result.push(arr.slice(i, i + size));
+    }
+    return result;
+  }
+
+  startUpload() {
+    let uploadPromise = [];
+    const spinner = ora('开始上传...').start();
+    this.waitUploadList.forEach((key) => {
+      uploadPromise.push(this.uploadFile(key))
+    });
+    Promise.all(uploadPromise)
+      .then(() => {
+        spinner.succeed('文件上传结束！');
+        this.startChangeMimeType()
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  startChangeMimeType() {
+    let mimePromise = [];
+    const spinner = ora('修改MimeType...').start();
+    this.chunk(this.waitChangeMimeTypeList, 100).forEach((ops) => {
+      mimePromise.push(this.changeMimeTypeBatch(ops))
+    });
+    Promise.all(mimePromise).then(() => {
+      spinner.succeed('修改文件MimeType结束！');
+      console.log('\n\n七牛云插件任务已结束...')
+    }).catch(err => {
+      console.log(err);
+    });
+  }
+
+  changeMimeTypeBatch(ops) {
+    return new Promise((resolve, reject) => {
+      this.bucketManager.batch(ops, (respErr, respBody, respInfo) => {
+        if (respErr) {
+          throw respErr;
+        }
+        if (respInfo.statusCode == 200) {
+          resolve(respInfo);
+        } else {
+          reject(respInfo);
+        }
+      });
+    });
+  }
+
+  getPathPrefixFiles() {
+    const spinner = ora(`获取指定前缀文件列表：${this.config.pathPrefix}`).start();
+    let options = {
+      limit: this.config.pathPrefixLimit,// 设置一个尽可能大的数，一次加载完所有的前缀文件列表
+      prefix: this.config.pathPrefix,
+    };
+    this.bucketManager.listPrefix(this.config.bucket, options, (respErr, respBody, respInfo) => {
+      if (respErr) {
+        console.log(respErr);
+        throw respErr;
+      }
+      if (respInfo.statusCode == 200) {
+        //如果这个nextMarker不为空，那么还有未列举完毕的文件列表，下次调用listPrefix的时候，
+        //指定options里面的marker为这个值
+        let nextMarker = respBody.marker;
+        if (nextMarker) {
+          throw '前缀文件列表未加载完全，尝试配置更大的pathPrefixLimit值，默认值为 99999999';
+        }
+        let items = respBody.items || [];
+        if (!items || items.length === 0) {
+          spinner.succeed(`获取到指定前缀文件列表为空：${this.config.pathPrefix}`);
+          this.startUpload()
+        } else {
+          spinner.succeed(`共获取到指定前缀文件：${items.length}`);
+          items.forEach((item) => {
+            spinner.succeed(item.key);
+            this.waitDeleteList.push(qiniu.rs.deleteOp(this.config.bucket, item.key));
+          });
+          this.startDelete()
+        }
+      } else {
+        spinner.fail(`获取指定前缀文件列表：${this.config.pathPrefix} 失败！`);
+        console.log(respInfo.statusCode);
+        console.log(respBody);
+      }
+    });
+  }
+
+  startDelete() {
+    let deletePromise = [];
+    const spinner = ora(`开始删除指定前缀文件列表：${this.config.pathPrefix}`).start();
+    this.chunk(this.waitDeleteList, 100).forEach((ops) => {
+      deletePromise.push(this.deleteBatch(ops));
+    });
+    Promise.all(deletePromise).then(() => {
+      spinner.succeed(`指定前缀文件列表删除完成！`);
+      this.startUpload()
+    }).catch((e) => {
+      console.log(e)
+    })
+  }
+
+  deleteBatch(ops) {
+    return new Promise((resolve, reject) => {
+      this.bucketManager.batch(ops, (respErr, respBody, respInfo) => {
+        if (respErr) {
+          console.log(respErr);
+          throw respErr;
+        }
+        if (respInfo.statusCode == 200) {
+          resolve(respInfo);
+        } else {
+          reject(respInfo);
+        }
+      });
+    });
+  }
+
+  uploadFile(filename, coverUploadToken) {
+    let key;
+    if (this.config.pathPrefix && this.config.pathPrefix != null && this.config.pathPrefix !== '') {
+      key = this.config.pathPrefix + filename;
+    } else {
+      key = filename;
+    }
+    const localFile = path.join(this.absolutePath || '', filename);
+    return new Promise((resolve, reject) => {
+      // 文件上传
+      const spinner = ora(`上传文件：${filename}`).start();
+      const uploadToken = coverUploadToken ? coverUploadToken : this.uploadToken;
+      this.formUploader.putFile(uploadToken, key, localFile, this.putExtra, (respErr, respBody, respInfo) => {
+          if (respErr) {
+            throw respErr;
+          }
+          if (respInfo.statusCode == 200) {
+            this.waitChangeMimeTypeList.push(
+              qiniu.rs.changeMimeOp(this.config.bucket, respBody.key, this.getMimeTypeByKey(respBody.key))
+            );
+            resolve(respInfo);
+            spinner.succeed(`文件：${key}，上传成功！`);
+          } else {
+            if (this.config.cover && (respInfo.status === 614 || respInfo.statusCode === 614)) {
+              spinner.fail(`文件：${key}，已存在，尝试覆盖上传！`);
+              resolve(this.uploadFile(filename, this.coverUploadFile(filename)));
+            } else {
+              spinner.fail(`文件：${key}，上传失败！`);
+              reject(respInfo);
+            }
+          }
+        }
+      );
+    });
+  }
+
+  getMimeTypeByKey(key) {
+    let start = key.lastIndexOf('.');
+    let ext;
+    if (start === -1) {
+      ext = '';
+    } else {
+      ext = key.substring(start);
+    }
+    return this.mimeType[ext] || 'application/octet-stream';
+  }
+
+  coverUploadFile(key) {
+    let options = {
+      scope: this.config.bucket + ':' + key
+    };
+    let putPolicy = new qiniu.rs.PutPolicy(options);
+    return putPolicy.uploadToken(this.mac);
+  }
+}
+
+module.exports = QiniuUploadPluginPro;
